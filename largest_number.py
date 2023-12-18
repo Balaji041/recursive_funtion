e18ec239d1a629ae9a189c43a6b305f26fdd3347
balaji <76476394+Balaji041@users.noreply.github.com>
2023-12-18 08:15:57 +0530

    Create largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,12 @@
+def largest(lists,Index):
+    print(max(lists[Index:]))
+lists=list(map(int,input().split(",")))
+Index=int(input())
+largest(lists,Index)
+"""
+input:10,7,2,5
+1
+output:7
+
+"""
+    
